23b1b7d1636de7068906c825f020bbc2f0199116
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2020-12-08 16:35:31 -0500

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,72 +1,39 @@
 input.onButtonPressed(Button.A, function () {
-    if (hours < 23) {
-        hours += 1
-    } else {
-        hours = 0
-    }
-})
-input.onButtonPressed(Button.AB, function () {
-    ampm = !(ampm)
-})
-input.onButtonPressed(Button.B, function () {
-    if (minutes < 59) {
-        minutes += 1
-    } else {
-        minutes = 0
-    }
-})
-input.onGesture(Gesture.Shake, function () {
-    adjust = hours
-    if (ampm) {
-        if (hours > 12) {
-            adjust = hours - 12
-        } else {
-            if (hours == 0) {
-                adjust = 12
-            }
-        }
-    }
+    adjust = heure
     time = "" + adjust
     time = "" + time + ":"
     time = "" + time + Math.trunc(minutes / 10)
     time = "" + time + Math.trunc(minutes % 10)
-    if (ampm) {
-        if (hours > 11) {
-            time = "" + time + "PM"
-        } else {
-            time = "" + time + "AM"
-        }
-    }
     basic.showString(time)
 })
-let hours = 0
 let minutes = 0
+let heure = 0
 let adjust = 0
 let time = ""
-let ampm = false
-ampm = false
 time = ""
 adjust = 0
-minutes = 0
-hours = 0
+heure = 15
+minutes = 38
+let strip = neopixel.create(DigitalPin.P0, 40, NeoPixelMode.RGB)
+strip.showColor(neopixel.colors(NeoPixelColors.Black))
 basic.forever(function () {
     basic.pause(60000)
     if (minutes < 59) {
         minutes += 1
     } else {
         minutes = 0
-        if (hours < 23) {
-            hours += 1
+        if (heure < 23) {
+            heure += 1
         } else {
-            hours = 0
+            heure = 0
         }
     }
 })
 basic.forever(function () {
-    if (hours == 11 && minutes == 19) {
-        music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    if (heure == 15 && minutes == 39) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
     }
-    if (hours == 15 && minutes == 11) {
-        music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    if (heure == 15 && minutes == 40) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
     }
 })
