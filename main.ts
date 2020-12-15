a84bf5af55ccf1b4825f19b08955ae32a6f3a8e6
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2020-12-15 07:59:49 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,8 @@ input.onButtonPressed(Button.A, function () {
     time = "" + time + Math.trunc(minutes % 10)
     basic.showString(time)
 })
+let range2: neopixel.Strip = null
+let range: neopixel.Strip = null
 let minutes = 0
 let heure = 0
 let adjust = 0
@@ -14,7 +16,7 @@ time = ""
 adjust = 0
 heure = 15
 minutes = 58
-let strip = neopixel.create(DigitalPin.P0, 40, NeoPixelMode.RGB)
+let strip = neopixel.create(DigitalPin.P12, 40, NeoPixelMode.RGB)
 strip.showColor(neopixel.colors(NeoPixelColors.Black))
 basic.forever(function () {
     basic.pause(60000)
@@ -30,10 +32,14 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (heure == 15 && minutes == 59) {
-        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    strip.setBrightness(255)
+    if (heure == 17 && minutes == 0) {
+        range = strip.range(0, 20)
+        range2 = strip.range(20, 20)
+        range.showColor(neopixel.colors(NeoPixelColors.Blue))
+        range2.showColor(neopixel.colors(NeoPixelColors.Red))
     }
-    if (heure == 16 && minutes == 1) {
+    if (heure == 7 && minutes == 30) {
         strip.showColor(neopixel.colors(NeoPixelColors.Black))
     }
 })
