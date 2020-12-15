ee4a84813fe740f6cef70ca6ae943d847313a6c5
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2020-12-15 08:11:34 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,10 +14,13 @@ let adjust = 0
 let time = ""
 time = ""
 adjust = 0
+// Ajuster l'heure lorsque vous démarrerez le micro:bit
 heure = 15
+// Ajuster les minutes lorsque vous démarrerez le micro:bit
 minutes = 58
 let strip = neopixel.create(DigitalPin.P12, 40, NeoPixelMode.RGB)
 strip.showColor(neopixel.colors(NeoPixelColors.Black))
+// Ne pas toucher à cette partie du programme car elle gère votre horloge.
 basic.forever(function () {
     basic.pause(60000)
     if (minutes < 59) {
@@ -32,13 +35,17 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
+    // Permet d'ajuster l'intensité lumineuse des DEL
     strip.setBrightness(255)
+    // Ajuster l'heure à laquelle vous désirez ouvrir les lumières.
     if (heure == 17 && minutes == 0) {
+        // Permet de sélectionner des couleurs d'éclairage.  Ici les DEL 0 à 19 sont bleues et les DEL 20 à 40 sont rouges.  Si on veut ajouter d'autres couleurs, il faut créer un nouvelle variable (ex : range3).SI on veut une seule couleur, on retire les blocs en lien avec "range2".
         range = strip.range(0, 20)
         range2 = strip.range(20, 20)
         range.showColor(neopixel.colors(NeoPixelColors.Blue))
         range2.showColor(neopixel.colors(NeoPixelColors.Red))
     }
+    // Ajuster l'heure à laquelle vous désirez fermer les lumières.
     if (heure == 7 && minutes == 30) {
         strip.showColor(neopixel.colors(NeoPixelColors.Black))
     }
